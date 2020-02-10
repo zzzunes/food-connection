db7124a7984deb873cab9b0ad0b4384712d185ea
Jared Taylor <jared.xyz@mailbox.org>
2020-02-10 01:36:42 -0500

    Initial search page

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { enableScreens } from 'react-native-screens';
 
+/* Components */
 import SearchPage from './components/SearchPage';
 import ProfilePage from './components/ProfilePage';
 import DataPage from './components/DataPage';
@@ -16,22 +18,13 @@ export default class App extends React.Component {
     render() {
         return (
             <NavigationContainer>
-                <Drawer.Navigator initialRouteName = "Search">
-                    <Drawer.Screen name = "Search" component = {SearchPage}/>
-                    <Drawer.Screen name = "Profile" component = {ProfilePage}/>
-                    <Drawer.Screen name = "Data" component = {DataPage}/>
-                    <Drawer.Screen name = "Settings" component = {SettingsPage}/>
+                <Drawer.Navigator initialRouteName = "Search" drawerStyle = {{ backgroundColor: '#d6cbff', width: 250 }} edgeWidth = {80}>
+                    <Drawer.Screen name = "Search"      component = {SearchPage}/>
+                    <Drawer.Screen name = "Profile"     component = {ProfilePage}/>
+                    <Drawer.Screen name = "Data"        component = {DataPage}/>
+                    <Drawer.Screen name = "Settings"    component = {SettingsPage}/>
                 </Drawer.Navigator>
             </NavigationContainer>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- components/SearchPage.js
+++ components/SearchPage.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { SearchBar } from 'react-native-elements';
 
-export default function SearchPage({navigation}) {
-    return (
-        <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Search Page</Text>
-        </View>
-    );
+export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    render() {
+        return (
+            <View style={styles.viewStyle}>
+                <SearchBar
+                    round
+                    searchIcon = {{ size: 20 }}
+                    onChangeText={(text) => this.setState({text})}
+                    placeholder = "Search for a food..."
+                    value = {this.state.text}
+                />
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    viewStyle: {
+      flex: 1,
+      backgroundColor: 'white',
+      marginTop: 24
+    },
+    textStyle: {
+      padding: 10,
+    },
+});
